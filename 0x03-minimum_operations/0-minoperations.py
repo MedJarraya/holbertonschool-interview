367d58b329ef62acf38fcbe5e9215d6f172f54a0
Mohamed Jarraya <mohamedjarraya@Mohameds-MacBook-Air.local>
2022-05-13 09:09:12 +0100

    fr

---
+++ 0x03-minimum_operations/0-minoperations.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""
+This Method calculates the fewest number of operations in the task
+"""
+
+
+def minOperations(n):
+    """This method returns the number of operations to copy paste in the task"""
+    if n <= 1:
+        return 0
+    operation = 0
+    divisor = 2
+    quotient = n
+    while quotient > 1:
+        if (quotient % divisor) == 0:
+            quotient = quotient // divisor
+            operation += divisor
+        else:
+            divisor += 1
+    return operation
