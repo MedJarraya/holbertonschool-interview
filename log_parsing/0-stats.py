597d1610e17a4c8647bea1d8c33377bf69eadc0c
MedJarraya <med.jarraya.02@gmail.com>
2024-03-27 13:27:10 -0400

    add

--- log_parsing/0-stats.py
+++ log_parsing/0-stats.py
@@ -1,15 +1,46 @@
 #!/usr/bin/python3
-import random
+"""
+this script reads stdin line by line and computes metrics
+"""
 import sys
-from time import sleep
-import datetime
 
-for i in range(10000):
-    sleep(random.random())
-    sys.stdout.write("{:d}.{:d}.{:d}.{:d} - [{}] \"GET /projects/260 HTTP/1.1\" {} {}\n".format(
-        random.randint(1, 255), random.randint(1, 255), random.randint(1, 255), random.randint(1, 255),
-        datetime.datetime.now(),
-        random.choice([200, 301, 400, 401, 403, 404, 405, 500]),
-        random.randint(1, 1024)
-    ))
-    sys.stdout.flush()
+
+def printS(fileSize, statusDict):
+    """ print """
+    print("File size: {:d}".format(fileSize))
+    for key in sorted(statusDict.keys()):
+        if statusDict[key] != 0:
+            print(
+                "{}: {:d}".format(
+                    key, statusDict[key]
+                )
+            )
+
+
+if __name__ == "__main__":
+    i = 0
+    statusDict = {
+        '200': 0,
+        '301': 0,
+        '400': 0,
+        '401': 0,
+        '403': 0,
+        '404': 0,
+        '405': 0,
+        '500': 0
+    }
+    fileSize = 0
+    try:
+        for line in sys.stdin:
+            keywords = line.split()
+            if len(keywords) >= 2:
+                if keywords[-2] in statusDict.keys():
+                    statusDict[keywords[-2]] += 1
+                fileSize += int(keywords[-1])
+                i += 1
+                if not i % 10:
+                    printS(fileSize, statusDict)
+        printS(fileSize, statusDict)
+    except KeyboardInterrupt:
+        printS(fileSize, statusDict)
+        raise
