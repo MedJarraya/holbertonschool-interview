ae2b028caeb4d644e41eab199f413a3062f41539
MedJarraya <med.jarraya.02@gmail.com>
2024-03-27 13:03:49 -0400

    add

---
+++ minimum_operations/0-minoperations.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+This method calculates the fewest number of operations!
+"""
+
+
+def minOperations(n):
+    if n <= 1:
+        return 0
+
+    operations = 0
+    divisor = 2
+    while n > 1:
+        while n % divisor == 0:
+            n //= divisor
+            operations += divisor
+        divisor += 1
+
+    return operations
+
+
+if __name__ == "__main__":
+    n = 4
+    print("Min number of operations to reach {} characters: {}".format(
+        n, minOperations(n)))
+
+    n = 12
+    print("Min number of operations to reach {} characters: {}".format(
+        n, minOperations(n)))
