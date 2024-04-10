4c2eec0cd0eff7a8366d7a4515d9ac2dc10f2f57
MedJarraya <med.jarraya.02@gmail.com>
2024-04-10 10:43:41 -0400

    new

--- starwars_api/0-starwars_characters.js
+++ starwars_api/0-starwars_characters.js
@@ -1,21 +1,34 @@
 #!/usr/bin/node
 /* This script prints characters
-    of Star Wars movie */
-    const request = require('request');
-    const process = require('process');
-    const Id = process.argv[2];
-    const url = `https://swapi.dev/api/films/${Id}/`;
-    request.get(url, async (_error, response, body) => {
-      for (const character of JSON.parse(body).characters) {
-        const people = await new Promise((resolve, reject) => {
-          request(character, (error, response, body) => {
-            if (error) {
-              reject(error);
-            } else {
-              resolve(JSON.parse(body).name);
-            }
-          });
-        });
-        console.log(people);
-      }
-    });
+   of Star Wars movie */
+   const request = require('request');
+   const process = require('process');
+   
+   const Id = process.argv[2];
+   const url = `https://swapi.dev/api/films/${Id}/`;
+   
+   request.get(url, async (error, response, body) => {
+     if (error) {
+       console.error(error);
+       return;
+     }
+   
+     const characters = JSON.parse(body).characters;
+     for (const character of characters) {
+       try {
+         const people = await new Promise((resolve, reject) => {
+           request(character, (error, response, body) => {
+             if (error) {
+               reject(error);
+             } else {
+               resolve(JSON.parse(body).name);
+             }
+           });
+         });
+         console.log(people);
+       } catch (error) {
+         console.error(error);
+       }
+     }
+   });
+   
