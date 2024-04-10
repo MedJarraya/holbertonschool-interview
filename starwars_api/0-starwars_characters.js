d24190344317c7e7ba70442bbff054cdd15994d4
MedJarraya <med.jarraya.02@gmail.com>
2024-04-10 10:31:16 -0400

    new

---
+++ starwars_api/0-starwars_characters.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+/*This script prints all characters of star wars movie*/
+    const request = require('request');
+    const process = require('process');
+    const Id = process.argv[2];
+    const url = `https://swapi.dev/api/films/${Id}/`;
+    request.get(url, async (_error, response, body) => {
+      for (const character of JSON.parse(body).characters) {
+        const people = await new Promise((resolve, reject) => {
+          request(character, (error, response, body) => {
+            if (error) {
+              reject(error);
+            } else {
+              resolve(JSON.parse(body).name);
+            }
+          });
+        });
+        console.log(people);
+      }
+    });
